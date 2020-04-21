459c84f255118fda31a0eccafd78ef4b96c19b87
s martinez <ciberpiranha@gmail.com>
2020-04-21 16:32:43 +0200

    Add code and axios as dependency

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-function sayHiTo(name) {
-  return `Hi, ${name}`;
+import axios from 'axios'
+
+export default {
+  async ajax (url, method, data) {
+    const token = document.head.querySelector('meta[name="csrf-token"]')
+    return axios({
+      method: method,
+      url: url,
+      data: data,
+      headers: { 'X-CSRF-TOKEN': token.content }
+    })
+  }
 }
-
-const message = sayHiTo('Bruno');
-
-console.log(message);
